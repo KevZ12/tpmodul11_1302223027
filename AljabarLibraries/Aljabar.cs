575394d8ba573363c08fcf0d7dbdf3a3a2123b95
KevZ12 <135513743+KevZ12@users.noreply.github.com>
2024-05-05 18:28:11 +0700

    menambahkan libraries aljabar

---
+++ AljabarLibraries/Aljabar.cs
@@ -0,0 +1,41 @@
+﻿namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public double[] AkarPersamaanKuadrat(double[] persamaan) {
+            double[] result = new double[2];
+            double a = persamaan[0];
+            double b = persamaan[1];    
+            double c = persamaan[2];
+
+            double akarDeterminan = Math.Sqrt(Math.Pow(b, 2) - 4 * a * c);
+
+            double x = (-b+ akarDeterminan)/2*a;
+            result[0] = x;
+            x = (-b - akarDeterminan) / 2 * a;
+            result[1] = x;
+
+
+            return result;
+
+        }
+
+        public double[] hasilKuadrat(double[] persamaan)
+        {
+            double[] result = new double[3];
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double x = Math.Pow(a,2);
+            result[0] = x;
+            x = 2*a*b;
+            result[1] = x;
+            x = Math.Pow(b, 2);
+            result[2] = x;
+
+
+            return result;
+            
+        }
+
+    }
+}
---
+++ test_Libraries/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AljabarLibraries;
+double[] persamaan1 = new double[] { 1,-3,-10} ;
+double[] persamaan2 = new double[] { 2,-3} ;
+Aljabar aljabar = new Aljabar();
+
+
+
+
+
+
+Console.WriteLine($"Akar persamaan kuadrat dari {persamaan1[0]}x^2" +
+                          $"{(persamaan1[1] >= 0 ? "+" : "")}{persamaan1[1]}x" +
+                          $"{(persamaan1[2] >= 0 ? "+" : "")}{persamaan1[2]} adalah x1 = {aljabar.AkarPersamaanKuadrat(persamaan1)[0]} " +
+                          $"dan x2 = {aljabar.AkarPersamaanKuadrat(persamaan1)[1]}");
+
+
+Console.WriteLine($"Hasil persamaan kuadrat dari a = {persamaan2[0]} dan b = {persamaan2[1]} adalah: {aljabar.hasilKuadrat(persamaan2)[0]}x^2" +
+                          $"{(aljabar.hasilKuadrat(persamaan2)[1] >= 0 ? "+" : "")}{aljabar.hasilKuadrat(persamaan2)[1]}x" +
+                          $"{(aljabar.hasilKuadrat(persamaan2)[2] >= 0 ? "+" : "")}{aljabar.hasilKuadrat(persamaan2)[2]}");
